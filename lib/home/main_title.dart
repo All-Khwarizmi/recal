857be12cc7aa039e0b93c8345dfe4494014efafc
Jason Suarez <swarecito@gmail.com>
2023-06-02 21:50:57 +0200

    👌 IMPROVE: Update home main title to display message and update ui in quizzes screen

--- lib/home/main_title.dart
+++ lib/home/main_title.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class MainTitle extends StatelessWidget {
   const MainTitle({
     super.key,
+    required this.text
   });
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      "Here are the topics you need to study today",
+      text,
       style: TextStyle(
         color: Colors.white,
         fontWeight: FontWeight.bold,
